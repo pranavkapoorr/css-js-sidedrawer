6132c9a747b91a8f9604614284cb7383d6d6e767
Pranav Kapoor <pranavkapoorr@gmail.com>
2019-06-20 22:41:28 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function openDrawer() {
+    document.getElementById("myDrawer").style.width = '250px';
+     document.getElementById("content").style.marginLeft = '250px';
+  }
+  function closeDrawer() {
+    document.getElementById("myDrawer").style.width = '0px';
+     document.getElementById("content").style.marginLeft = '0px';
+  }
