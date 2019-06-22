22afafebd757d75184f5af21b81144610588d3d7
Pranav Kapoor <pranavkapoorr@gmail.com>
2019-06-22 16:24:22 -0400

    anim

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function openDrawer() {
-    document.getElementById("myDrawer").style.width = '250px';
-     document.getElementById("content").style.marginLeft = '250px';
-  }
-  function closeDrawer() {
-    document.getElementById("myDrawer").style.width = '0px';
-     document.getElementById("content").style.marginLeft = '0px';
-  }
+   //document.getElementById("myDrawer").style.width = '70%';
+   document.getElementById("content").style.marginLeft = '70%';
+   document.getElementById("myBackdrop").classList.add('backdrop');
+   document.getElementById("myDrawer").classList.add('animate-open');
+}
+function closeDrawer() {
+   document.getElementById("content").style.marginLeft = '0px';
+   document.getElementById("myBackdrop").classList.remove('backdrop');
+   document.getElementById("myDrawer").classList.remove('animate-open');
+   //document.getElementById("myDrawer").style.width = '0px';
+}
